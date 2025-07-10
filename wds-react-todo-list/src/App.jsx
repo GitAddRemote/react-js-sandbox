ddb3be4e0b30586eea46558d735d537eee25263e
Demian <demian@gitaddremote.com>
2025-07-10 00:43:16 -0400

    Raw functional app'

--- wds-react-todo-list/src/App.jsx
+++ wds-react-todo-list/src/App.jsx
@@ -1,4 +1,5 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
   const [newItem, setNewItem] = useState("");
@@ -7,25 +8,54 @@ export default function App() {
   function handleSubmit(e) {
     e.preventDefault();
 
+    setTodoList(prevList => {
+      return [...prevList, {id: crypto.randomUUID(), title: newItem, completed: false}];
+    });
+    setNewItem("");
+  }
+  
+  function handleToggle(id, completed) { 
+    setTodoList(prevList => {
+      return prevList.map(item => {
+        if (item.id === id) {
+          return {...item, completed: completed};
+        }
+        return item;
+      });
+    });
   }
 
+  function handleDelete(id) {
+    setTodoList(prevList => {
+      return prevList.filter(item => item.id !== id);
+    });
+  } 
+
   return <>
-    <form className="new-item-form">
+    <form onSubmit={handleSubmit} className="new-item-form">
     <div className="form-row">
       <label htmlFor="item">Item</label>
-      <input type="text" id="item" />
+      <input 
+        value={newItem}
+        onChange={e => setNewItem(e.target.value)}
+        type="text" 
+        id="item" />
     </div>
     <button className="btn">Add</button>
   </form>
   <h1>ToDo List</h1>
   <ul>
-    <li>
-      <label>
-        <input type="checkbox" />
-        Item 1
-      </label>
-      <button className="btn btn-danger">Delete</button>
-    </li>
+    {todoList.map(item => (
+      <>
+        <li key={item.id}>
+          <label>
+            <input type="checkbox" checked={item.completed} onChange={() => handleToggle(item.id, item.completed)} />
+            {item.title}
+          </label>
+          <button className="btn btn-danger" onClick={() => handleDelete(item.id)}>Delete</button>
+        </li>
+      </>
+    ))}
   </ul>
   </>
 }
