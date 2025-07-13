199932db3feb26888e40cf41622e7ccef9a51a94
Demian <demian@gitaddremote.com>
2025-07-12 21:36:07 -0400

    Add timeout code

---
+++ rkc-use-effect-timer/src/components/Alert.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+export function Alert({ message }) {
+
+    const [alertDone, setAlertDone] = useState(false);
+
+    useEffect(() => {
+        console.log("Starting Alert Timer!");
+        setTimeout(function () {
+            console.log("Timer Expired!");
+            setAlertDone(true);
+        }, 2000);
+        
+        // return () => clearTimeout(timer); // Cleanup the timer on unmount
+    }, []);
+
+    return (
+        <>
+            {!alertDone && <p>Relax, you've still some time left!</p>}
+            {alertDone && <p>Time's up!</p>}
+        </>
+    );
+}
