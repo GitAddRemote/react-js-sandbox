5f3b9c19242f76cc93d6963cb3fad2a25a7a6823
Demian <demian@gitaddremote.com>
2025-07-15 01:23:36 -0400

    Fix syntax error

--- wds-react-todo-list/babel.config.mjs
+++ wds-react-todo-list/babel.config.mjs
@@ -1,7 +1,9 @@
 export default {
   presets: [
-    '@babel/preset-env',[
+    '@babel/preset-env',
+    [
       '@babel/preset-react',
-      { runtime: 'automatic' }   // ← this tells Babel to auto‑import the JSX helpers
-  ]]
-}
+      { runtime: 'automatic' }
+    ]
+  ]
+};
