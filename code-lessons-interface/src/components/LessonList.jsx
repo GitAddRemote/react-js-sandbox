25b59a4c19faf1a0b1d43f50b98caaf5a62bf408
Demian <demian@gitaddremote.com>
2025-07-22 23:40:13 -0400

    Code review cleanup

--- code-lessons-interface/src/components/LessonList.jsx
+++ code-lessons-interface/src/components/LessonList.jsx
@@ -3,7 +3,7 @@ import { LessonItem } from './LessonItem'
 export function LessonList(props) {
     return (
         <ul>
-            { props.lessonList.map(lesson => (
+            { props.lessonList?.map(lesson => (
                 <LessonItem key={lesson.id} title={lesson.title} subTitle={lesson.subTitle} url={`/lessons/${lesson.id}`} description={lesson.description} />
             ))}
         </ul>
